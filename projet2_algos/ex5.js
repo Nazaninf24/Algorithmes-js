9f0001fdc7dd7b77ded377850be0b4998609593b
Nazaninf24 <nazaninf24@gmail.com>
2020-11-11 17:18:24 +0100

    Fourth

--- projet2_algos/ex5.js
+++ projet2_algos/ex5.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 
 fs.readFile(process.argv[2], "utf8", (error, data) => {
   numbers = data.split(" ").filter((item) => !isNaN(item)).map(Number);
-  selectionSort(numbers, numbers.length);
+  numberMatch(numbers, numbers.length);
 });
 
-const selectionSort = (numbers) => {
+const numberMatch = (numbers) => {
   let k = 17;
   let n = 0;
 
